85743082a69f21604cb04db299c96a9d84591959
Andrew Melnychuk <amelnychukoseen@gmail.com>
2018-08-23 15:12:50 -0700

    used sklearn to split the data

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
 
 import warnings
 
@@ -13,23 +14,22 @@ def show_digit(digit, label, num):
     plt.title("Training: {}".format(label[num]))
     plt.show()
 
-def get_data(limit=None):
-    """Gets mnist data from lazyprogrammers' hit hub.
 
-    :param limit:
-        (int) the maximum amount of samples your would like from mnist
 
-    :return: np.ndarray (N,D), np.ndarray (N,)
-        X, Y mnist features and labels
+def get_data():
+    """Gets mnist data from sklearn library. Shuffles and splits it
+
+    :return: np.ndarray (N,D), np.ndarray (N,) np.ndarray (N,D), np.ndarray (N,)
+        Xtrain (np.ndarray (N,D) ),
+        Ytrain, (np.ndarray (N,) ),
+        Xtest (np.ndarray (N,D) ),
+        Ytest, (np.ndarray (N,) ),
     """
-    data, labels = load_digits(return_X_y=True)
+    data, targets = load_digits(return_X_y=True)
 
-    X, Y = shuffle(data, labels)
-    if limit is not None:
-        xSize = len(X)
-        if limit > xSize:
-            warnings.warn("Limit of {} exceeds data sample size of {}".format(limit, xSize))
-        X, Y = X[:limit], Y[:limit]
+    X, Y = shuffle(data, targets)
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=.1, train_size=.9)
 
-    return X, Y
+
+    return Xtrain, Ytrain, Xtest, Ytest
 
